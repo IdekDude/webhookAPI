1e8b23d9f203fabc59083433b2bcbd9f6c47c2ab
Chris Obora <74634965+IdekDude@users.noreply.github.com>
2021-07-16 12:35:24 -0500

    Done

--- handlers/helpers.go
+++ handlers/helpers.go
@@ -1,16 +1,11 @@
 package handlers
 
 import (
-	"fmt"
-	"github.com/joho/godotenv"
 	"os"
 	"strings"
 )
 
 func LoadWebhooks(key string) []string {
-	if err := godotenv.Load(".env"); err != nil {
-		fmt.Println(err)
-	}
 	webhook := os.Getenv(key)
 
 	hookArray := strings.Split(webhook, ",")
